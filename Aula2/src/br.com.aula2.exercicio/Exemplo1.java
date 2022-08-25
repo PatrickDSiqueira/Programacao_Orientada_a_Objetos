c3897ded01ddb7b1c6d0b47c91cca4925e1be1c3
Patrick Dias Siqueira <12114788@aluno.newtonpaiva.br>
2022-08-25 20:20:35 -0300

    create Exemplo1

---
+++ Aula2/src/br.com.aula2.exercicio/Exemplo1.java
@@ -0,0 +1,13 @@
+/**
+ * Exemplo1
+ */
+public class Exemplo1 {
+    public static void main(String[] args) {
+        int x, y, num, soma;
+        x=10;
+        y=11;
+        num = x++;
+        System.out.println("x: " + x + "\ny:" + y + "num:");
+
+    }
+}
