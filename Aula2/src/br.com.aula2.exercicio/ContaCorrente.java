f07e8fc162ccd1fef0353ed3b6373ce21116b04c
Patrick Dias Siqueira <12114788@aluno.newtonpaiva.br>
2022-08-25 21:38:48 -0300

    create class ContaCorrente

---
+++ Aula2/src/br.com.aula2.exercicio/ContaCorrente.java
@@ -0,0 +1,34 @@
+/**
+ * ContaCorrente
+ */
+public class ContaCorrente {
+    private long numero;
+    private int agengia;
+    private Double saldo;
+
+    ContaCorrente(long num, int ag){
+        numero = num;
+        agengia = ag;
+        saldo = 0.;
+    }
+
+
+    public void Depositar (double valor){
+        saldo += valor;
+    }
+
+    public void Sacar (double valor){
+        if(valor <= saldo){
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insulficiente para o saque");
+        }
+    }
+
+    public double ConsultaSaldo (){
+        return saldo;
+    }
+
+
+ 
+}
