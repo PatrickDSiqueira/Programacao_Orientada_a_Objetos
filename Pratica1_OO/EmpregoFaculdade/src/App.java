84ffcdb459dbe57390dbbd04f64ce70f9fc7281c
Patrick Siqueira <psique.estuda@gmail.com>
2022-08-28 22:35:29 -0300

    Changing the main scope for the App

--- Pratica1_OO/EmpregoFaculdade/src/App.java
+++ Pratica1_OO/EmpregoFaculdade/src/App.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
+import br_com_pratica1_questao2_empregoFaculdade.EmpregadoFaculdade;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner ler = new Scanner(System.in);
+
+        String nome;
+        double salario;
+        int horasAula;
+         
+        System.out.println("Digite o nome do funcionario : ");
+            nome  = ler.next();
+
+        System.out.println("Digite o valor do salario deste funcionario : ");
+            salario = ler.nextDouble();
+
+         System.out.println("Digite as Horas Aulas deste funcionario : ");
+            horasAula = ler.nextInt();
+
+        EmpregadoFaculdade empregado1  = new EmpregadoFaculdade(nome, salario, horasAula);
+
+        System.out.println("O valor do gasto com esse funcionário é : " + empregado1.getGastos());
+        System.out.println(empregado1.getInfo());
+      
+        ler.close();
     }
 }
--- Pratica1_OO/EmpregoFaculdade/src/br_com_pratica1_questao2_empregoFaculdade/EmpregadoFaculdade.java
+++ Pratica1_OO/EmpregoFaculdade/src/br_com_pratica1_questao2_empregoFaculdade/EmpregadoFaculdade.java
@@ -1,37 +1,9 @@
 package br_com_pratica1_questao2_empregoFaculdade;
 
-import java.util.Scanner;
-
 public class EmpregadoFaculdade {
     private String nome;
     private double salario;
     private int horasAula;
-
-    public static void main(String[] args) {
-
-        Scanner ler = new Scanner(System.in);
-
-        String nome;
-        double salario;
-        int horasAula;
-         
-        System.out.println("Digite o nome do funcionario : ");
-            nome  = ler.next();
-
-        System.out.println("Digite o valor do salario deste funcionario : ");
-            salario = ler.nextDouble();
-
-         System.out.println("Digite as Horas Aulas deste funcionario : ");
-            horasAula = ler.nextInt();
-
-        EmpregadoFaculdade empregado1  = new EmpregadoFaculdade(nome, salario, horasAula);
-
-        System.out.println("O valor do gasto com esse funcionário é : " + empregado1.getGastos());
-        System.out.println(empregado1.getInfo());
-      
-        ler.close();
-    }
-
     
     public EmpregadoFaculdade(String nome, double salario, int horasAula) {
         this.nome = nome;
@@ -39,12 +11,12 @@ public class EmpregadoFaculdade {
         this.horasAula = horasAula;
     }
 
-    double getGastos(){
+    public double getGastos(){
         double bonus = 40;
         return this.salario + (bonus * horasAula);
     } 
 
-    String getInfo(){
+    public String getInfo(){
         return "nome:" + this.nome + " com salario " + getGastos();
 
     }
