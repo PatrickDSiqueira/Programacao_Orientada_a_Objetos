d92822cd4aa04c3b84e9a92c7bf3db79d6563be8
Patrick Siqueira <psique.estuda@gmail.com>
2022-08-29 11:45:15 -0300

    Resolved menu exit option

--- Pratica1_OO/Concessionaria/src/App.java
+++ Pratica1_OO/Concessionaria/src/App.java
@@ -8,22 +8,23 @@ public class App {
         Textos textos = new Textos();
         Scanner ler = new Scanner(System.in);
         String opcao = "";
+        boolean saidaMenu = false;
         
         do {
             System.out.print(textos.getMenuOpcao());
                 opcao = ler.next();
             switch (opcao) {
                 case "1":
-                System.out.print("Nome :" );
+                System.out.print("Nome : " );
                 String nome = ler.next(); 
 
-                System.out.print("\nCPF :" );
+                System.out.print("\nCPF : " );
                 String cpf = ler.next();  
 
-                System.out.print("\nNumero de Telefone :" );
+                System.out.print("\nNumero de Telefone : " );
                 long numeroTelefone = ler.nextLong(); 
 
-                System.out.print("\n Ano de Nascimento :" );
+                System.out.print("\nAno de Nascimento : " );
                 int anoNascimeto = ler.nextInt();
 
                 Cliente cliente1 = new Cliente(nome, cpf, numeroTelefone, anoNascimeto);
@@ -38,12 +39,18 @@ public class App {
                 case "3":
 
                     break;
+                case "0":
+                        saidaMenu = true;
+                        System.out.println("Programa Finalizado!");
+                        System.out.println(saidaMenu);
+
+                    break;
 
                 default:
-                    System.out.println();
+                    System.out.println("Opção inválida");
                     break;
             }
-        } while ( opcao != "0" );
+        } while ( saidaMenu == false );
         ler.close();
     }
 }
--- Pratica1_OO/Concessionaria/src/br_com_pratica1_questao1_concessionaria/Textos.java
+++ Pratica1_OO/Concessionaria/src/br_com_pratica1_questao1_concessionaria/Textos.java
@@ -4,7 +4,7 @@ public class Textos {
     private String menuOpcao ;
 
     public Textos() {
-        this.menuOpcao =  "1 - Cadastrar Cliente. \n 2 - Cadastrar Carro. \n3 - Realizar Transação. \nOpção - >";
+        this.menuOpcao =  "1 - Cadastrar Cliente \n2 - Cadastrar Carro \n3 - Realizar Transação \n0 - Sair\nOpção - >";
     }
 
 
