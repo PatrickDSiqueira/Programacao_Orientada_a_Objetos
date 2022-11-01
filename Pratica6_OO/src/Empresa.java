25bdb7026f227177c4a14bb53d5444da9fc56054
Patrick Dias Siqueira <12114788@aluno.newtonpaiva.br>
2022-11-01 19:32:18 -0300

    Pratica 6 iniciada

---
+++ Pratica6_OO/src/Empresa.java
@@ -0,0 +1,6 @@
+public class Empresa {
+    private String nome;
+    private String CNPJ;
+private String ;
+private String ;
+}
---
+++ Pratica6_OO/src/Funcionario.java
@@ -0,0 +1,7 @@
+public class Funcionario  extends Pessoa{
+    private String departamento ;
+private Double  salario;
+private String dataEntradaBanco;
+private String RG;
+    private boolean presenteEmpresa ;
+}
